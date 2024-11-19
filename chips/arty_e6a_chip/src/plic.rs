df8995b206e332110a18e22d456cfedb6420de45
Eugene Shamis <eugene.shamis@amd.com>
2024-11-19 12:14:06 -0500

    Added a wrapper around PlicRegisters for bounded access

--- chips/arty_e6a_chip/src/plic.rs
+++ chips/arty_e6a_chip/src/plic.rs
@@ -7,9 +7,11 @@
 use kernel::utilities::StaticRef;
 use sifive::plic::{Plic, PlicRegisters};
 
+const TOTAL_INTS: usize = 129;
+
 pub const PLIC_BASE: StaticRef<PlicRegisters> =
     unsafe { StaticRef::new(0x0040_0000 as *const PlicRegisters) };
 
-pub type E6APlic = Plic<129>;
+pub type E6APlic = Plic<TOTAL_INTS>;
 
 pub static mut PLIC: E6APlic = Plic::new(PLIC_BASE);
